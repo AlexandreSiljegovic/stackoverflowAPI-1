dffe6d058c126991acba5d05833caf7058e3a4f6
RJPinoy <rejenaligoradarsonval@gmail.com>
2024-02-09 14:31:51 +0100

    axios api added to symfony

--- skeleton/src/Controller/RegistrationController.php
+++ skeleton/src/Controller/RegistrationController.php
@@ -2,11 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Entity\Member;
-use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -17,30 +16,32 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
+        // Parse JSON data from the request body
+        $jsonData = json_decode($request->getContent(), true);
+
+        // Create a new Member entity
         $member = new Member();
         $member->setRegistrationDate(new \DateTime());
-        $form = $this->createForm(RegistrationFormType::class, $member);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $member->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $member,
-                    $form->get('plainPassword')->getData()
-                )
-            );
+        // Set Member properties from JSON data
+        $member->setUsername($jsonData['username']);
+        $member->setEmail($jsonData['email']);
+        $member->setFirstName($jsonData['first_name']);
+        $member->setLastName($jsonData['last_name']);
 
+        // Hash the password
+        $member->setPassword(
+            $userPasswordHasher->hashPassword(
+                $member,
+                $jsonData['password']
+            )
+        );
 
-            $entityManager->persist($member);
-            $entityManager->flush();
-            // do anything else you need here, like send an email
+        // Persist the Member entity
+        $entityManager->persist($member);
+        $entityManager->flush();
 
-            return $this->redirectToRoute('app_register');
-        }
-
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form->createView(),
-        ]);
+        // Return a JSON response
+        return new JsonResponse(['message' => 'User registered successfully'], JsonResponse::HTTP_CREATED);
     }
 }
